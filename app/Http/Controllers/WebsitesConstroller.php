049ce9afec544303f6b7a38966ca6f4cfac35b47
Mellaoui <Modijavelin@gmail.com>
2021-08-28 15:17:31 +0100

    first commit

---
+++ app/Http/Controllers/WebsitesConstroller.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Website;
+use Illuminate\Http\Request;
+
+class WebsitesConstroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function subscribe(Website $website){
+        $user = User::find(Auth::id());
+        $website->users()->save($user);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Website $website)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Website $website)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Website $website)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Website  $website
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Website $website)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\WebsitesConstroller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::middleware('auth:sunctum')->post('/websites/{website}/subscribe',[WebsitesConstroller::class, 'subscribe']);
+
+Route::resource('/websites/{website}/posts',PostsController::class);
+
+
+
